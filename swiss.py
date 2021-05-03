6bb73170fb1c6fa2022597ebad3cb987503a6631
alban_favre <fav.alban2@gmail.com>
2021-05-03 13:57:04 +0200

    final fix rotation

--- swiss.py
+++ swiss.py
@@ -18,18 +18,21 @@ swissFraction = 1
 width = math.floor(int(dimlist[0]) / swissFraction)
 height = math.floor(int(dimlist[1]) / swissFraction)
 
-factor = 0.0005 # fraction of degree betrween each point in file, pifometre
+# x = (47.5-45)/3001
+# y = (7.5-5)/3001
+# the two factor are the same, executive decision to use the same factor for both
+factor_longitude_latitude = 0.00083305564
+factor_big_swiss = 0.01
 maxZ = 4783  # hard coded, but it's the max in file
-minZ = 134  # hard coded, but it's the min in file
+minZ = 134  # hard coded, but it's the min in file, never used
 genf = 370  # that is what the teacher said, genf is at 370
 climate_crisis = False
 
-latitude_base_angle = 48
-longitude_base_angle = 5
-earth_radius = 6371000
+latitude_base_angle = 45
+longitude_base_angle = 7.5
+earth_radius = 6371009
 
 # ------------------------look up table
-
 # ColorConst
 colorConst = {
     'BLUE': (0.0, 0.5, 0.75),
@@ -41,7 +44,7 @@ colorConst = {
 
 # LookupTable Settings
 color_min = 0
-color_max = 4783
+color_max = maxZ
 color_water = color_min
 nb_colors = color_max - color_min
 
@@ -58,12 +61,12 @@ color_lookuptable.AddRGBPoint(color_max, *colorConst['WHITE'])
 color_lookuptable.Build()
 
 # --------------------------------------------
-
 points = vtk.vtkPoints()
 cells = vtk.vtkCellArray()
 
 points_ids = [[0 for x in range(width)] for y in range(height)]
-points_ints = [[0 for x in range(width)] for y in range(height)]
+points_ints = [[0 for x in range(width)] for y in
+               range(height)]  # [0][0] point en haut a gauche [max][max] en bas a droite
 
 # Generate the colors for each point based on the color map
 colors = vtk.vtkUnsignedCharArray()
@@ -75,7 +78,8 @@ print('get ints from file')  # we do this here and not below, because we need to
 for i in range(height):
     line = f.readline().split()
     for j in range(width):
-        points_ints[j][i] = int(line[(width-1)-j]) # rotate back with the [j][i] instead of [i][j], we also mirror the width values with (width-1)-j
+        points_ints[j][i] = int(line[(
+                                                 width - 1) - j])  # rotate back with the [j][i] instead of [i][j], we also mirror the width values with (width-1)-j
 
 print('get points and color for each int')
 for i in range(width):
@@ -83,16 +87,17 @@ for i in range(width):
 
         pointTransform = vtk.vtkTransform()
         pointTransform.RotateY(
-            longitude_base_angle + (i * factor))  # confusing but that is a rotation x based on snowman
-        pointTransform.RotateX(latitude_base_angle - (j * factor))  # confusing but that is latitute
+            longitude_base_angle + (
+                        i * factor_longitude_latitude))  # to get x, we rotate around Y, we start right and go left
+        pointTransform.RotateX(latitude_base_angle - (
+                    j * factor_longitude_latitude))  # to get y we rotate around X, we start up and go down
         pointTransform.Translate(0, 0, (earth_radius + points_ints[i][j]))
         position = 3 * [0.0]
         position = pointTransform.GetPosition()
 
         # do points
         points_ids[i][j] = points.InsertNextPoint(position)  # round earth
-        #points_ids[i][j] = points.InsertNextPoint(i,j,points_ints[i][j])  # flat earth
-
+        # points_ids[i][j] = points.InsertNextPoint(i,j,points_ints[i][j])  # flat earth
 
         # do scalar color
         if (i > 0 and j > 0 and i < (width - 1) and (j < (height - 1)) and (
@@ -140,10 +145,9 @@ print('create polydata')
 polydata = vtk.vtkPolyData()
 polydata.SetPoints(points)
 polydata.SetPolys(cells)
-polydata.GetPointData().SetScalars(colors)  # apparently you can bind a lut instead of doing this
+polydata.GetPointData().SetScalars(colors)  # apparently you can bind a lookup table instead of doing this
 
 # ------------------------------------------------ mapper + actor
-
 print('create mapper and actor')
 
 # Create a mapper
@@ -154,9 +158,7 @@ polydataActor = vtk.vtkActor()
 polydataActor.SetMapper(polydataMapper)
 
 # ---------------------------------------------- put things in scene
-
 print('start rendering')
-
 ren1 = vtk.vtkRenderer()
 ren1.AddActor(polydataActor)
 ren1.SetBackground(0.8, 0.5, 0.5)
@@ -164,6 +166,13 @@ ren1.SetBackground(0.8, 0.5, 0.5)
 renWin = vtk.vtkRenderWindow()
 renWin.AddRenderer(ren1)
 renWin.SetSize(800, 800)
+
+# for some reason, our polydata is upside down (probably because I prefer the 0,0 point up left instead of down left)
+# therefore we rotate it and everything is fixed
+polydataTransform = vtk.vtkTransform()
+polydataTransform.RotateZ(180)
+polydataActor.SetUserTransform(polydataTransform)  # set
+renWin.Render()
 
 iren = vtk.vtkRenderWindowInteractor()
 iren.SetRenderWindow(renWin)
